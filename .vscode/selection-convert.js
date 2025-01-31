5adb8e10c772a33cfd7944caae55adfd32fec10e
Rick Pfahl <pfahlr@gmail.com>
2025-01-31 03:34:10 -0500

    implement multiple filters

--- .vscode/selection-convert.js
+++ .vscode/selection-convert.js
@@ -1,39 +1,49 @@
-//------------------------------------------------------------------------------
-// Author: Rick Pfahl
-// Requires: https://github.com/jeff-hykin/selection-convert.git
-// Description: filter definitions for Selection Convert Module by jeff-hykin
-// Date: 2025-
-// Software used in filters:
-// - Boxes [https://boxes.thomasjensen.com] 
-//
+/*
+ ╔══════════════════════════════════════════════════════════════════════════════╗
+ ║                                                                              ║
+ ║  Author: Rick Pfahl                                                          ║
+ ║  Description: Filter definitions for Selection Convert Module by jeff-hykin  ║
+ ║  Requires: [https://github.com/jeff-hykin/selection-convert.git]             ║
+ ║  Date: Jan 2025                                                              ║
+ ║  Software used in filters:                                                   ║
+ ║  - Boxes [https://boxes.thomasjensen.com]                                    ║
+ ║                                                                              ║
+ ╚══════════════════════════════════════════════════════════════════════════════╝
 
+*/
 const { execSync } = require('child_process');
 
 //Filter Export Functions
 
-// -- function for Boxes - gradientbox
+// -- function for Boxes - gradientbox-----------------------------------------
+
 selectionToGradient = function(text) {
   var result = runFilter(text, 'gradientbox');
   return result;
 }
 
+selectionToIdiotBox = function(text) {
+  var result = runFilter(text, 'idiotbox');
+  return result; 
+}
+  
+selectionToDoubleLineBox = function(text) {
+  var result = runFilter(text, 'doublelinebox');
+  return result; 
+}
+
 // --/ END Filter Export Functions /--
 
 runFilter = function(text, design) {
-  //return "blah"+text+"blah";
-  var command = getFilter(text,design);
-  //return command;
+  var command = getFilterCommand(text,design);
   var result = "";
   result = execSync(command);
   return result.toString();
 }
 
-getFilter = function(text,design) {
+getFilterCommand = function(text,design) {
   var command = "echo \""+text+"\" | boxes -d "+design+" -p h1 -s 80 -i none -n UTF-8"
   return command;
 }
 
-
-module.exports = { selectionToGradient };
-
-
+module.exports = { selectionToGradient, selectionToDoubleLineBox, selectionToIdiotBox };
